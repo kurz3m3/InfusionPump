fdefc6da2d96939cf6e75c8ed14cc6f444c8829d
Harvey <Jak3grason@gmail.com>
2022-11-14 20:03:59 -0500

    Added thermistor

--- InfusionPump.ino
+++ InfusionPump.ino
@@ -13,7 +13,10 @@ int redLED = 4;
 int button = 3; // button set to pin 3
 int motor = 5; // motor set to pin 6
 int buttonInput = 0; // buttonInput set to 0 meaning not pressed
+int thermPin = A0;
+int thermValue = 0;
 
+static bool pressedStatus;
 
 /*
 * Setup function.
@@ -23,6 +26,7 @@ void setup() {
   pinMode(greenLED,OUTPUT);
   pinMode(redLED,OUTPUT);
   pinMode(button,INPUT);
+  pinMode(thermPin,INPUT);
   Serial.begin(9600);
   Serial.println("Program Reset"); 
 }
@@ -33,6 +37,7 @@ void setup() {
 */
 void loop() {
   pumpMain();
+  Serial.print("Temp:" + String(thermOutput()));
 }
 
 
@@ -48,6 +53,13 @@ void pumpMain(){
     pumpOn();
     delay(5000); // wait for 5 seconds with motor ON
     pumpOff();
+
+    if(thermOutput()>=80){
+      pressedStatus = false;
+      Serial.println("EMERGENCY SHUTDOWN: TEMP EXCEEDED 80");
+      pumpOff();
+    }
+
     delay(20000); // Wait 20 seconds
   }
   else{
@@ -62,7 +74,7 @@ void pumpMain(){
 *
 */
 void pumpOn(){
-  Serial.println("Motor on");
+  Serial.println("  Motor on  ");
   analogWrite(motor,255); // turn motor to max speed
   digitalWrite(greenLED,HIGH); // green LED ON
   digitalWrite(redLED,LOW);// red LED OFF
@@ -75,7 +87,7 @@ void pumpOn(){
 * Wait 20 seconds
 */
 void pumpOff(){
-  Serial.println("Motor off");
+  Serial.println("  Motor off");
   analogWrite(motor,0); // Turn motor OFF
   digitalWrite(greenLED,LOW); // turn green LED OFF
   digitalWrite(redLED,HIGH); // turn red LED ON
@@ -89,7 +101,6 @@ void pumpOff(){
 *
 */
 bool isPressed(){
-  static bool pressedStatus;
   buttonInput = digitalRead(button); //. read button input
   
   if(buttonInput == 1 && pressedStatus == true){ // if button pressed and its already been pressed before
@@ -105,3 +116,28 @@ bool isPressed(){
   return pressedStatus;
 }
 
+
+float thermOutput(){
+  thermValue=analogRead(thermPin);
+
+  int R25 = 10000; //Ohms
+	float a1 = 3.354016E-3;
+	float b1 = 2.56985E-4;
+	float c1 = 2.620131E-6;
+	float d1 = 6.383091E-8;
+
+  float voltage = float(thermValue)*(5000.0/1024.0); //units are milivolts
+  float Rdivider = 10000; //Resistance in Ohms of the other resistor.
+  float Rtherm = Rdivider*((5000.0/voltage) -1); //Voltage divider equation
+	
+  //Thermistor resistance-to-temperature conversion equation (from datasheet)
+	float Tkelvin = 1/(a1 + b1*log(Rtherm/R25) \
+		+ c1*log(Rtherm/R25)*log(Rtherm/R25) \
+		+ d1*log(Rtherm/R25)*log(Rtherm/R25)*log(Rtherm/R25));
+
+  //Temperature conversions
+	float Tcelsius = Tkelvin - 273.15;
+	float Tfahren = (Tcelsius*(9.0/5.0))+32.0;
+
+  return Tfahren;
+}
